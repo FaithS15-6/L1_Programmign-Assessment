a19da69416a7664ea6d24b4a0efdb4b552af9ff8
FaithS15-6 <78391318+FaithS15-6@users.noreply.github.com>
2021-06-01 10:37:56 +1200

    base

---
+++ 00_sandpit.py
@@ -0,0 +1,12 @@
+import random
+from typing import MutableMapping
+
+num_1 = random.randrange(1, 100)
+num_2 = num_1 / 100
+num_3 = num_1 + 0.01
+num_4 = num_1 / 10 +2
+
+print(num_1)
+print(num_2)
+print(num_3)
+print(num_4)
