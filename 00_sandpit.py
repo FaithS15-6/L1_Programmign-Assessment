32b0305b20a522df1dd3ec4a4679e64bc343935a
FaithS15-6 <78391318+FaithS15-6@users.noreply.github.com>
2021-06-01 10:52:31 +1200

    sample

--- 00_sandpit.py
+++ 00_sandpit.py
@@ -5,8 +5,10 @@ num_1 = random.randrange(1, 100)
 num_2 = num_1 / 100
 num_3 = num_1 + 0.01
 num_4 = num_1 / 10 +2
+num_5 = num_2 + 12 / 0.9
 
 print(num_1)
 print(num_2)
 print(num_3)
 print(num_4)
+print(num_5)
