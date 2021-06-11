32c4515c1e61c48963ec0c4dc7cf6d56cd880d6c
FaithS15-6 <78391318+FaithS15-6@users.noreply.github.com>
2021-06-11 13:19:47 +1200

    Update 02_CH_userchoice_v2.py

--- 02_CH_userchoice_v2.py
+++ 02_CH_userchoice_v2.py
@@ -1,2 +1,19 @@
-02_CH_userchoice_v2.py
+# Functions go here
+def choice_checker (question):
+
+    valid = False
+    while not valid :
+
+        # Ask user for choice
+        response = input(question)
+
+        if response == "r" or response == "rock":
+            return response
+
+# Main routine goes here
+
+# Ask user for choice and check if it's valid
+
+user_choice = choice_checker("Choose rock / paper / scissors (r/p/s): ")
+
 
