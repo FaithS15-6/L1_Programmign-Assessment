569f9923d7b5d95c351eba65afcfe000a0c8c684
FaithS15-6 <78391318+FaithS15-6@users.noreply.github.com>
2021-06-11 13:19:44 +1200

    Update 02_CH_userchoice.py

--- 02_CH_userchoice.py
+++ 02_CH_userchoice.py
@@ -1,20 +1,25 @@
 import random
 
-ch_list = ["true", "false"]
-rps_list = ["1", "50"]
+num_1 = random.randrange(1, 100)
+num_2 = num_1 - 50
+num_3 = num_1 + 4
+num_4 = num_1 / -10 +2
+num_5 = num_2 + 3
+num_6 = num_2 + 2
 
-def true_false(question):
-    valid = False
-    while not valid:
-        response = input(question).lower()
+numlist =[num_1, num_2, num_3, num_4, num_5, num_6]
 
-        if response == "true" or response == "t":
-            response = "true"
-            return response
+print(num_1)
+print(num_2)
+print(num_3)
+print(num_4)
+print(num_5)
+print(num_6)
 
-        elif response == "false" or response == "f":
-            response = "f"
-            return response
+if num_1 > num_3:
+    print("")
+if num_3 < num_1:
+    print("")
 
-        else:
-            print("please answer true/false")
+print()
+print("Chose the highest number of of the numbers given above")
