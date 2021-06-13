0ad42f04b15f66a048836b9e46d734bf99598c00
FaithS15-6 <78391318+FaithS15-6@users.noreply.github.com>
2021-06-14 11:04:49 +1200

    Update 01 CHOOSE HIGHEST_base component.py

--- 01 CHOOSE HIGHEST_base component.py
+++ 01 CHOOSE HIGHEST_base component.py
@@ -1,1 +1,34 @@
-00 CHOOSE HIGHEST_base component
+import random
+
+ # Functions go here 
+ # List of valid player response
+ # check rounds played
+    # Fuctions go here
+def check_rounds():
+        while True:
+            response = input("How many rounds: ")
+
+            round_error = "Please type either <enter> or an " \
+                        "integer that is more than 0\n"
+
+            # If infinite mode not chosen, check response
+            # Is an interger that is more than 0
+            if response != "":
+                try:
+                    response = int(response)
+
+                    # If response is too low, go back to start of loop
+
+                    if response < 1:
+                        print(round_error)
+                        continue
+
+                except ValueError:
+                    print(round_error)
+                    continue
+
+        return response
+
+ # check number entered is valid (true/false)
+
+ # Functions go here
