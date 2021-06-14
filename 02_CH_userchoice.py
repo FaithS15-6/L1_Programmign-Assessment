0898d836032decfeb32e118df66f36ac314bb6ff
FaithS15-6 <78391318+FaithS15-6@users.noreply.github.com>
2021-06-15 10:22:35 +1200

    Update 02_CH_userchoice.py

--- 02_CH_userchoice.py
+++ 02_CH_userchoice.py
@@ -1,36 +1,33 @@
 import random
 
-num_1 = random.randrange(1, 100)
-num_2 = num_1 - 50
+num_1 = random.randrange(1, 50)
+num_2 = num_1 * 15
 num_3 = num_1 + 4
 num_4 = num_1 / -10 +2
 num_5 = num_2 + 3
 num_6 = num_2 +num_3
-num_7 = num_2 - num_4
-num_8 = num_7 * num_1
-num_9 = num_1 + 5
-num_10 = num_9-num_4
 
-numlist =[num_1, num_2, num_3, num_4, num_5, num_6, num_7, num_8, num_9,num_10]
+numlist =[num_1, num_2, num_3, num_4, num_5, num_6,]
 
+print("One")
 print(num_1)
+print()
+print("Two")
 print(num_2)
+print()
+print("Three")
 print(num_3)
+print()
+print("Four")
 print(num_4)
+print()
+print("Five")
 print(num_5)
+print()
+print("Six")
 print(num_6)
-print(num_7)
-print(num_8)
-print(num_9)
-print(num_10)
-
-if num_1 > num_3:
-    print("")
-if num_3 < num_1:
-    print("")
-
 
 print()
-print("Chose the highest number of of the numbers given above")
+print("Chose the highest number from the list of numbers above")
 
 
