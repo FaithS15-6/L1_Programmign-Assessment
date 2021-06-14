5ab5bf3abace73cfae03ec4ae7d3cefabadb0c36
FaithS15-6 <78391318+FaithS15-6@users.noreply.github.com>
2021-06-15 10:56:01 +1200

    Update 00_sandpit.py

--- 00_sandpit.py
+++ 00_sandpit.py
@@ -1,17 +1,5 @@
 import random
 
-num_1 = random.randrange(1, 100)
-num_2 = num_1 / 100
-num_3 = num_1 + 0.01
-num_4 = num_1 / 10 +2
-num_5 = num_2 + 12 / 0.9
-num_6 = num_2 + 0.65
-
-print(num_1)
-print(num_2)
-print(num_3)
-print(num_4)
-print(num_5)
-
-if num_1 > num_3:
-    print("")
+list1 = [456, 700, 200]
+print ("Max value element :", max(list1))
+print ("Min value element :", min(list1))
