5553c5931a93ca76cff8e43d82e560b19ebdff10
FaithS15-6 <78391318+FaithS15-6@users.noreply.github.com>
2021-06-15 10:56:04 +1200

    Update 01 CHOOSE HIGHEST_base component.py

--- 01 CHOOSE HIGHEST_base component.py
+++ 01 CHOOSE HIGHEST_base component.py
@@ -1,34 +1,57 @@
 import random
 
- # Functions go here 
- # List of valid player response
- # check rounds played
+    # Functions go here 
+    # List of valid player response
+    
+num_1 = random.randrange(1, 50)
+num_2 = num_1 * 15
+num_3 = num_1 + 4
+num_4 = num_1 / -10 +2
+num_5 = num_2 + 3
+num_6 = num_2 +num_3
+
+list1 = [num_1, num_2, num_3, num_4, num_5, num_6 ]
+print ("Max value element :", max(list1))
+print ("Min value element :", min(list1))
+
+print("One")
+print(num_1)
+print()
+print("Two")
+print(num_2)
+print()
+print("Three")
+print(num_3)
+print()
+print("Four")
+print(num_4)
+print()
+print("Five")
+print(num_5)
+print()
+print("Six")
+print(num_6)
+
+print()
+
+    # check rounds played
+    # Functions go here
+def choice_checker (number):
+
+    valid = False
+    while not valid :
+
+        # Ask user for choice
+        response = input(number)
+
+        if response == "true" or response == "t":
+            return response
+
+# Main routine goes here
+
+# ask user for answer and check that it is correct.
+
+user_choice = choice_checker("Choose the highest number from the list: one / two / three / four / five / six: ")
+
+
     # Fuctions go here
-def check_rounds():
-        while True:
-            response = input("How many rounds: ")
-
-            round_error = "Please type either <enter> or an " \
-                        "integer that is more than 0\n"
-
-            # If infinite mode not chosen, check response
-            # Is an interger that is more than 0
-            if response != "":
-                try:
-                    response = int(response)
-
-                    # If response is too low, go back to start of loop
-
-                    if response < 1:
-                        print(round_error)
-                        continue
-
-                except ValueError:
-                    print(round_error)
-                    continue
-
-        return response
-
- # check number entered is valid (true/false)
-
- # Functions go here
