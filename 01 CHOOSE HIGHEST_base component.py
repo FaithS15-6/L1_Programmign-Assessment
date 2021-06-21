57fab7d91fe64879458c9304a07e7e83208b3c4e
FaithS15-6 <78391318+FaithS15-6@users.noreply.github.com>
2021-06-22 11:06:05 +1200

    Update 01 CHOOSE HIGHEST_base component.py

--- 01 CHOOSE HIGHEST_base component.py
+++ 01 CHOOSE HIGHEST_base component.py
@@ -1,12 +1,32 @@
 import random
 
-    # Functions go here 
-    # List of valid player response
-    # Ask user if they have played befor
-    # Game instructions on how to play
+# Functions go here 
+def intcheck(question):
+    while True:
+        response = input(question)
+
+        round_error = "Please type either <enter> or an " \
+                      "integer that is more than 0\n"
+
+        # If infinite mode not chosen, check response
+        # Is an integer that is more than 0
+        if response != "":
+            try:
+                response = int(response)
+
+                # If response is too low, go back to start of loop
+
+            except ValueError:
+                print(round_error)
+                continue
+
+        return response    
+# Ask user if they have played before
+# Game instructions on how to play
 # Checks number of rounds
 
-  # develop random numbers players can   
+  # develop random numbers players can***  
+# 
 num_1 = random.randrange(1, 50)
 num_2 = num_1 * 15
 num_3 = num_2 + 4
@@ -15,45 +35,20 @@ num_5 = num_2 + 3
 num_6 = num_5 +num_3
 
 list1 = [num_1, num_2, num_3, num_4, num_5, num_6 ]
-print("One")
-print(num_1)
-print()
-print("Two")
-print(num_2)
-print()
-print("Three")
-print(num_3)
-print()
-print("Four")
-print(num_4)
-print()
-print("Five")
-print(num_5)
-print()
-print("Six")
-print(num_6)
+
+print(*list1, sep= ", ")
+
 
 print()
 
-    # check rounds played
-    # Functions go here
-def choice_checker (number):
-
-    valid = False
-    while not valid :
-
-        # Ask user for choice
-        response = input(number)
-
-        if response == "true" or response == "t":
-            return response
 
 # Main routine goes here
 
 # ask user for answer and check that it is correct.
 
-user_choice = choice_checker("Choose the highest number from the list ")
+user_choice = intcheck("Choose the highest number from the list ")
 # Number list, 
+correct = max(list1)
 
 # Prints out correct answers (Highest and Lowest when player is done)
 print ("Max value element :", max(list1))
