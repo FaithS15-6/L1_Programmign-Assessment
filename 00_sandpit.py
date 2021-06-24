b534fd3e1205afe27804d6d71edcade24c05b418
FaithS15-6 <78391318+FaithS15-6@users.noreply.github.com>
2021-06-24 14:50:21 +1200

    Update 00_sandpit.py

--- 00_sandpit.py
+++ 00_sandpit.py
@@ -1,13 +1,3 @@
-# creating empty list
-lis = []
+import random
 
-# user enters the number of elements to put in list
-count = int(input('How many numbers? '))
-
-# iterating till count to append all input elements in list
-for n in range(count):
-    number = int(input('Enter number: '))
-    lis.append(number)
-
-# displaying largest element
-print("Largest element of the list is :", max(lis))
+# main routine goes here
