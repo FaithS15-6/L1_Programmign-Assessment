d7249610596f7a3b6a44196a35ed5acc29b8d90e
FaithS15-6 <78391318+FaithS15-6@users.noreply.github.com>
2021-06-28 15:31:48 +1200

    Update 01 CHOOSE HIGHEST_base component.py

--- 01 CHOOSE HIGHEST_base component.py
+++ 01 CHOOSE HIGHEST_base component.py
@@ -3,7 +3,27 @@ import random
 
 # Functions go here...
 
-# checks that users answer yes/no to a question
+def intcheck(question):
+    while True:
+        response = input(question)
+
+        round_error = "Please type either <enter> or an " \
+                      "integer that is more than 0\n"
+
+        # If infinite mode not chosen, check response
+        # Is an integer that is more than 0
+        if response != "":
+            try:
+                response = int(response)
+
+                # If response is too low, go back to start of loop
+
+            except ValueError:
+                print(round_error)
+                continue
+
+        return response    
+
 def yes_no(question):
     valid = False
     while not valid:
@@ -20,10 +40,7 @@ def yes_no(question):
         else:
             print("Please answer yes / no")
 
-# Checks that users enter a number a number between two end points
 
-# Ask user if they have played before
-# Game instructions on how to play
 def instructions():
     print()
     print("**** How to play ****")
@@ -33,19 +50,33 @@ def instructions():
     return ""
 
 # Main Routine goes here...
+
+
+
 played_before = yes_no("Have you played the game before? ")
 
 if played_before == "no":
     instructions()
-
 print()
 
 # Checks number of rounds
+rounds_played = 0
+rounds_lost = 0
+rounds_won = 0
 
-chose_instructions = "please chose the highest and lowest number in the list"
+game_summary = []
+
+chose_instructions = "please chose the highest"
+
+# Ask user for number of rounds. <enter> for infinite mode 
+rounds =intcheck (question)
+
+end_game ="no"
+while end_game == "no":
+
 # develop random numbers players can***
 # 
-num_1 = random.randrange(1, 50)
+    num_1 = random.randrange(1, 50)
 num_2 = num_1 * 15
 num_3 = num_2 + 4
 num_4 = num_3 +2
@@ -64,8 +95,9 @@ print()
 
 # Main routine goes here
 
+# ask user for answer and check that it is correct.
 
-# ask user for answer and check that it is correct for higest list.
+
 user_ans = intcheck("Choose the highest number from the list ")
 correct_answer = max(list1)
 
@@ -73,7 +105,7 @@ if user_ans == correct_answer:
     print ("Well done")
 else:
     print("Oops")
-# ask user for answer and check if it is correct for lowest list.
+
 user_ans = intcheck("Choose the lowest number from the list")
 correct_answer = min(list1)
 
@@ -89,5 +121,3 @@ correct = max(list1)
 # Prints out correct answers (Highest and Lowest when player is done)
 print ("Max value element :", max(list1))
 print ("Min value element :", min(list1))
-
-# Functions go here
