ac66794fa08a21c56b7167fd731feff30af51bef
FaithS15-6 <78391318+FaithS15-6@users.noreply.github.com>
2021-06-29 10:41:03 +1200

    fixing rounds

--- 01 CHOOSE HIGHEST_base component.py
+++ 01 CHOOSE HIGHEST_base component.py
@@ -49,27 +49,28 @@ def instructions():
     print()
     return ""
 
+
+
 # Main Routine goes here...
 
 
+answered_before = yes_no("Have you played the game before? ")
 
-played_before = yes_no("Have you played the game before? ")
-
-if played_before == "no":
+if answered_before == "no":
     instructions()
 print()
 
-# Checks number of rounds
-rounds_played = 0
-rounds_lost = 0
-rounds_won = 0
+# Checks number of question
+question_answered = 0
+question_wrong = 0
+question_correct = 0
 
 game_summary = []
 
 chose_instructions = "please chose the highest"
 
-# Ask user for number of rounds. <enter> for infinite mode 
-rounds =intcheck (question)
+# Ask user for number of question. <enter> for infinite mode 
+question =intcheck ("How many questions:")
 
 end_game ="no"
 while end_game == "no":
@@ -77,47 +78,48 @@ while end_game == "no":
 # develop random numbers players can***
 # 
     num_1 = random.randrange(1, 50)
-num_2 = num_1 * 15
-num_3 = num_2 + 4
-num_4 = num_3 +2
-num_5 = num_2 + 3
-num_6 = num_5 +num_3
+    num_2 = num_1 * 15
+    num_3 = num_2 + 4
+    num_4 = num_3 +2
+    num_5 = num_2 + 3
+    num_6 = num_5 +num_3
 
-list1 = [num_1, num_2, num_3, num_4, num_5, num_6] 
+    list1 = [num_1, num_2, num_3, num_4, num_5, num_6] 
 
-# shuffle numbers in list.
-random.shuffle(list1)
-print(list1)
-
-
-print()
+    # shuffle numbers in list.
+    random.shuffle(list1)
+    print(list1)
 
 
-# Main routine goes here
+    print()
 
-# ask user for answer and check that it is correct.
+    # ask user for answer and check that it is correct.
 
 
-user_ans = intcheck("Choose the highest number from the list ")
-correct_answer = max(list1)
+    user_ans = intcheck("Choose the highest number from the list ")
+    correct_answer = max(list1)
 
-if user_ans == correct_answer:
-    print ("Well done")
-else:
-    print("Oops")
+    if user_ans == correct_answer:
+        print ("Well done")
+    else:
+        print("Oops")
 
-user_ans = intcheck("Choose the lowest number from the list")
-correct_answer = min(list1)
+    user_ans = intcheck("Choose the lowest number from the list")
+    correct_answer = min(list1)
 
-if user_ans == correct_answer:
-    print ("Correct!!")
-else:
-    print("Damn It")
-# ways to win
+    if user_ans == correct_answer:
+        print ("Correct!!")
+    else:
+        print("Damn It")
+    # ways to win
 
-# Number list, 
-correct = max(list1)
+    # Number list, 
+    correct = max(list1)
 
-# Prints out correct answers (Highest and Lowest when player is done)
-print ("Max value element :", max(list1))
-print ("Min value element :", min(list1))
+    
+    if question!= "" and question_answered >= question - 1:
+        break
+    
+    # Prints out correct answers (Highest and Lowest when player is done)
+    print ("Max value element :", max(list1))
+    print ("Min value element :", min(list1))
