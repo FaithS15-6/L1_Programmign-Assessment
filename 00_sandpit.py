713b1604039add0e64fdb2fb19fd07b9f2d9ee75
FaithS15-6 <78391318+FaithS15-6@users.noreply.github.com>
2021-06-28 14:18:49 +1200

    Update 00_sandpit.py

--- 00_sandpit.py
+++ 00_sandpit.py
@@ -1,3 +1,20 @@
 import random
 
-# main routine goes here
+
+import random
+
+num_list = [1, 3, 5, 31, 24, 3.4]
+
+random.shuffle(num_list)
+
+
+print(num_list)
+user_ans = int(input("What is the highetst number in the  list above? "))
+correct_answer = max(num_list)
+
+if user_ans == correct_answer:
+    print("Well done")
+else: 
+    print("Oops")
+
+
