65df8c7c16d50245746e07c8138dea8f43274926
FaithS15-6 <78391318+FaithS15-6@users.noreply.github.com>
2021-06-30 09:56:49 +1200

    Create 09_CH&L_score.py

---
+++ 09_CH&L_score.py
@@ -0,0 +1,26 @@
+# CH&L componet 6 - scring system 
+
+# correct answers will be calculated (total - correct - wrong)
+question_answered = 0
+question_wrong = 0
+question_correct = 0
+
+# results for testing puposes
+test_results = ["correct", "correct", "wrong", "wrong"]
+
+#play game
+for item in test_results:
+    question_answered += 1
+
+    print(test_results)
+
+print()
+# quick calculations (stats)
+question_correct = question_answered - question_wrong 
+
+# end of game Statistics
+print()
+print('*End Game Summary*')
+print("Correct Answers: {} \t|\t Wrong Answers: {}".format(question_correct, question_wrong))
+print()
+print("Thanks 4 playing*")
