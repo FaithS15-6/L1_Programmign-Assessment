0668ff2b288925baffbd3283360a83a2413dfaf1
Faith Sama <78391318+FaithS15-6@users.noreply.github.com>
2021-07-09 02:04:34 +0000

    01: game history and game statistics

--- 01_base_component.py
+++ 01_base_component.py
@@ -1,7 +1,7 @@
 import random
 
-
 # Functions go here...
+
 
 def intcheck(question):
     while True:
@@ -22,7 +22,8 @@ def intcheck(question):
                 print(round_error)
                 continue
 
-        return response    
+        return response
+
 
 def yes_no(question):
     valid = False
@@ -49,11 +50,12 @@ def instructions():
     print()
     return ""
 
+
 # Add character above and below statement to make it stand out.
 def statement_generator(statement, decoration):
     sides = decoration * 3
 
-    statement = "{} {} {}".format(sides,statement, sides)
+    statement = "{} {} {}".format(sides, statement, sides)
     top_bottom = decoration * len(statement)
 
     print(top_bottom)
@@ -61,7 +63,9 @@ def statement_generator(statement, decoration):
     print(top_bottom)
     return ""
 
-statement_generator("Welcome to the Choose Highest and Lowest Number game", "*")
+
+statement_generator("Welcome to the Choose Highest and Lowest Number game",
+                    "*")
 
 # Main Routine goes here...
 answered_before = yes_no("Have you played the game before? ")
@@ -79,101 +83,119 @@ game_summary = []
 
 chose_instructions = "please chose the highest and lowest from list"
 
-# Ask user for number of question. <enter> for infinite mode 
-question =intcheck ("How many questions would you like to answer?  Each question has TWO parts!!:")
+# Ask user for number of question. <enter> for infinite mode
+question = intcheck(
+    "How many questions would you like to answer?  Each question has TWO parts!!:"
+)
 
-end_game ="no"
+end_game = "no"
 while end_game == "no":
 
     # End game when exit code (xxx)is entered.
-  
 
-# loops while questions answered is more than questions requested
+    # loops while questions answered is more than questions requested
 
     # Start of game play loop
-    question_answered +=1
+    question_answered += 1
 
     # Rounds Heading
     print()
-    if question =="":
+    if question == "":
         heading = "Continues Mode :Question {}".format(question_answered)
     else:
-        heading = "Question {} of Question {}".format(question_answered, question)
+        heading = "Question {} of Question {}".format(question_answered,
+                                                      question)
 
         print(heading)
 
+# End Game if exit code is typed
 
- # End Game if exit code is typed
+# end game is round entered is finished.
 
-        # end game is round entered is finished.
-            
 # develop random numbers players choose
 
     num_1 = random.randrange(1, 100)
     num_2 = num_1 * 15
     num_3 = num_2 + 10
-    num_4 = num_3 +2
+    num_4 = num_3 + 2
     num_5 = num_2 + 7
-    num_6 = num_5 +num_3
+    num_6 = num_5 + num_3
 
-    list1 = [num_1, num_2, num_3, num_4, num_5, num_6] 
+    list1 = [num_1, num_2, num_3, num_4, num_5, num_6]
 
     # shuffle numbers in list.
     random.shuffle(list1)
     print(list1)
 
-
     print()
 
     # ask user for answer and check that it is correct.
 
-#Check answers for highest value
-    user_ans = intcheck("Choose the highest number from the list ")
+    #Check answers for highest value
+    user_ans = intcheck("Choose the highest number from the list: ")
     correct_answer = max(list1)
 
     if user_ans == correct_answer:
         statement_generator("Well done", "!")
         result = "right"
-        
+
     else:
         statement_generator("Oops", "x")
         result = "wrong"
-      
 
     part = "a"
-    question_summary = "Question {}{}: {}".format(question_answered, part, result)
+    question_summary = "Question {}{}: {}".format(question_answered, part,
+                                                  result)
     game_summary.append(question_summary)
     print()
     statement_generator("Done part one", "~")
     print()
-# check answer for lowest value
-    user_ans = intcheck("Choose the lowest number from the list")
+    # check answer for lowest value
+    user_ans = intcheck("Choose the lowest number from the list:")
     correct_answer = min(list1)
 
     if user_ans == correct_answer:
-        statement_generator ("Correct", "!")
+        statement_generator("Correct", "*")
         result = "right"
-      
+
     else:
         statement_generator("Uh oh", "x")
         result = "wrong"
-    
-    
+
     part = "b"
-    question_summary = "Question {}{}: {}".format(question_answered, part, result)
+    question_summary = "Question {}{}: {}".format(question_answered, part,
+                                                  result)
     game_summary.append(question_summary)
     print()
-    statement_generator("Done part two", "*") 
+    statement_generator("Done part two", "~")
     print()
 
     if question != "" and question_answered >= question:
         break
 
     # Prints out correct answers (Highest and Lowest when player is done)
-    print ("Highest Value :", max(list1))
-    print ("Lowest Value :", min(list1))
+    print("Highest Value :", max(list1))
+    print("Lowest Value :", min(list1))
 
+# Calculate game Statistics
+percent_right = question_correct / question_answered * 100
+percent_wrong = question_wrong / question_answered * 100
+
+print("***Game History***")
 print()
 print(game_summary)
 
+print()
+# displays game stats with % values to the nearest whole number 
+print("****Game Statistics****")
+print("Correct: {}, ({:.0f}%)\nWrong: {}, ({:.0f}%)".format(question_correct,
+                                                            percent_right, 
+                                                            question_wrong,
+                                                            percent_wrong))
 
+# End of game statements*
+print()
+print("****End Game Summary*****")
+print("Correct: {} \t|\t Wrong: {} ".format(question_correct, question_wrong))
+print()
+print("Thanks 4 playing")
